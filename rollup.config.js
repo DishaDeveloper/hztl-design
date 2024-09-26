f28f1ed92ee637428f69a86c6fb993019e9f261a
Disha <disha.patel@horizontal.com>
2024-09-26 16:13:51 +0530

    Update the component for Xmcloud setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,103 @@
-import typescript from 'rollup-plugin-typescript2';
+import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
-import { terser } from 'rollup-plugin-terser';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import esbuild from 'rollup-plugin-esbuild'; 
+import glob from 'fast-glob';
+import { rimraf } from 'rimraf';
+import packageJson from './package.json';
 
-export default {
-  input: 'src/index.tsx', // Entry point
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'esm',
-      sourcemap: true,
-    },
-    {
-      file: 'dist/index.umd.js',
-      format: 'umd',
-      name: 'MyReactPackage',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript(),
-    postcss({
-      plugins: [],
-      minimize: true,
+const outputDir = 'dist';
+
+async function getEntries() {
+  const componentEntries = await glob('src/components/**/index.ts');
+  return ['src/index.tsx', ...componentEntries];
+}
+
+async function buildConfig() {
+  const entries = await getEntries();
+
+  const external = [
+    'react',
+    'react-dom',
+    'react/jsx-runtime',
+    new RegExp('react-icons/*'),
+    ...Object.keys({
+      ...packageJson.dependencies,
+      ...packageJson.peerDependencies,
     }),
-    terser(),
-  ],
-  external: ['react', 'react-dom'], // Exclude React from the bundle
-};
+  ];
+
+  return {
+    input: entries,
+    output: [
+      {
+        format: 'es',
+        dir: `${outputDir}/esm`,
+        entryFileNames: '[name].mjs',
+        preserveModules: true,
+        sourcemap: true,
+      },
+      {
+        format: 'cjs',
+        dir: `${outputDir}/cjs`,
+        entryFileNames: '[name].cjs',
+        preserveModules: true,
+        sourcemap: true,
+      },
+      {
+        format: 'es',
+        file: `${outputDir}/index.mjs`, // Output for ES module
+        sourcemap: true,
+
+      },
+      {
+        format: 'cjs',
+        file: `${outputDir}/index.js`, // Output for CommonJS module
+        sourcemap: true,
+      },
+    ],
+    external,
+    plugins: [
+      cleanOutputDir(),
+      postcss(),
+     
+      esbuild({
+        sourceMap: false,
+        minify: false,
+        target: 'esnext',
+      }),
+      babel({
+        babelHelpers: 'bundled',
+        presets: ['@babel/preset-react'],
+        exclude: 'node_modules/**',
+      }),
+      generateDts(),
+    ],
+    onwarn(warning, warn) {
+      if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+        return;
+      }
+      warn(warning);
+    },
+  };
+}
+
+function cleanOutputDir() {
+  return {
+    name: 'clean-output-dir',
+    async buildStart() {
+      await rimraf(outputDir);
+    },
+  };
+}
+
+function generateDts() {
+  return {
+    name: "generate-dts",
+    async closeBundle() {
+      await `tsc -p tsconfig.build.json --outDir ${outputDir}/types`;
+    },
+  };
+}
+
+
+export default buildConfig();
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,3 @@
 export * from "./Components/MyComponent";
 export * from "./Components/FeatureComponent";
+export { TestComponent } from "./Components/TestComponent";
